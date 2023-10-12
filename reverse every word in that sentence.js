c77e5e8585daa4c753f5e92081e877a1329a7b4d
SaiBommedi <109013504+SaiBommedi@users.noreply.github.com>
2023-10-12 14:22:36 +0530

    Update reverse every word in that sentence.js

--- reverse every word in that sentence.js
+++ reverse every word in that sentence.js
@@ -1,1 +1,10 @@
 
+const str = prompt();
+const reverseSentence = str => {
+   const arr = str.split(" ");
+   const reversed = arr.map(el => {
+      return el.split('').reverse().join("");
+   });
+   return reversed.join(" ");
+};
+console.log(reverseSentence(str));
